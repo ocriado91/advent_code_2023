6bbbfffff20c54e39bdacfa847d8526917f513f7
Oscar Criado de la Torre <ocriado91@gmail.com>
2023-12-06 15:42:34 +0100

    Add solutions of AoC-Day#2

--- src/advent_code_2023.py
+++ src/advent_code_2023.py
@@ -4,7 +4,12 @@ Advent of Code (https://adventofcode.com/) is a set
 of small daily code challenges like an advent Calendar
 '''
 
-from tools.tools import load_data, extract_digits, word_to_value
+from tools.tools import load_data,\
+                        extract_digits,\
+                        word_to_value,\
+                        extract_round_results,\
+                        check_game_results,\
+                        check_minimum_cubes
 
 
 def solution_day_one_1_part_a(data: list) -> int:
@@ -30,7 +35,32 @@ def solution_day_one_1_part_b(data: list) -> int:
     return sum_data
 
 
-if __name__ == '__main__': # pragma: no cover
+def solution_day_two_part_a(data: list) -> int:
+    '''
+    Solution of the AoC Day#2 (Part A)
+    '''
+
+    results = extract_round_results(data)
+    valid_games = check_game_results(results)
+    return sum(valid_games)
+
+
+def solution_day_two_part_b(data: list) -> int:
+    '''
+    Solution of the AoC Day#2 (Part B)
+    '''
+
+    results = extract_round_results(data)
+    return check_minimum_cubes(results)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    # Day 1
     day_one_data = load_data("data/puzzle_input_day_one.dat")
-    print(f"Solution of day one (part A) = {solution_day_one_1_part_a(day_one_data)}")
-    print(f"Solution of day one (part B) = {solution_day_one_1_part_b(day_one_data)}")
+    print(f"Day one (A) = {solution_day_one_1_part_a(day_one_data)}")
+    print(f"Day one (B) = {solution_day_one_1_part_b(day_one_data)}")
+
+    # Day 2
+    day_two_data = load_data("data/puzzle_input_day_two.dat")
+    print(f"Day two (A) = {solution_day_two_part_a(day_two_data)}")
+    print(f"Day two (B) = {solution_day_two_part_b(day_two_data)}")
--- test/test_advent_code.py
+++ test/test_advent_code.py
@@ -8,7 +8,13 @@ from advent_code_2023 import load_data,\
                              extract_digits,\
                              word_to_value,\
                              solution_day_one_1_part_a,\
-                             solution_day_one_1_part_b
+                             solution_day_one_1_part_b,\
+                             solution_day_two_part_a,\
+                             solution_day_two_part_b
+
+from tools.tools import extract_round_results,\
+                        check_game_results,\
+                        check_minimum_cubes
 
 
 def test_load_data():
@@ -60,6 +66,67 @@ def test_word_to_value_change_digits():
     assert word_to_value(data[4]) == 42
     assert word_to_value(data[5]) == 14
     assert word_to_value(data[6]) == 76
+
+
+def test_extract_round_results():
+    '''
+    AoC Day#2
+    ----------------------
+    Test correct extraction of round results
+    '''
+    data = load_data("test/data/game_test.dat")
+    results = extract_round_results(data)
+    assert len(results) == 5
+
+
+def test_check_game_results():
+    '''
+    Aoc Day#2
+    --------------------
+    Test correct check of game results dictionary
+    '''
+
+    data = load_data("test/data/game_test.dat")
+    results = extract_round_results(data)
+    valid_games = check_game_results(results)
+    assert len(valid_games) == 3
+
+
+def test_check_minimum_cubes():
+    '''
+    AoC Day#2
+    ------------------
+    Test power minimum cubes power function
+    '''
+
+    data = load_data("test/data/game_test.dat")
+    results = extract_round_results(data)
+    power_games = check_minimum_cubes(results)
+    assert power_games == 2286
+
+
+def test_solution_day_two_part_a():
+    '''
+    AoC Day#2
+    ------------------
+    Test solution day 2 (part A)
+    '''
+
+    data = load_data("test/data/game_test.dat")
+    result = solution_day_two_part_a(data)
+    assert result == 8
+
+
+def test_solution_day_two_part_b():
+    '''
+    AoC Day#2
+    ------------------
+    Test solution day 2 (part B)
+    '''
+
+    data = load_data("test/data/game_test.dat")
+    result = solution_day_two_part_b(data)
+    assert result == 2286
 
 
 def test_solution_day_one_part_a():
